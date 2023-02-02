18d425393aab101fcf42bbe83155db6a71ef6808
chanki <coldair426@gmail.com>
2023-02-02 23:52:56 +0900

    programmers Lv.0 최빈값 구하기게시글 코드

---
+++ programmers Lv.0 최빈값 구하기/obj-values.js
@@ -0,0 +1,5 @@
+const melon = { NewJeans: 'Ditto', 윤하: '사건의 지평선', 지코: '쌔핑' };
+console.log(Object.keys(melon)); // [ 'NewJeans', '윤하', '지코' ]
+
+const melon = { NewJeans: 'Ditto', 윤하: '사건의 지평선', 지코: '쌔핑' };
+console.log(Object.values(melon)); // [ 'Ditto', '사건의 지평선', '쌔핑' ]
