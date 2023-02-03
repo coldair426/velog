c948cb211167564666787b0e9f23063ecab22a04
chanki <coldair426@gmail.com>
2023-02-03 23:52:16 +0900

    programmers Lv.0 최빈값 구하기 게시글 코드

--- programmers Lv.0 최빈값 구하기/다른사람풀이2.js
+++ programmers Lv.0 최빈값 구하기/다른사람풀이2.js
@@ -6,14 +6,11 @@ const solution = (array) => {
     }),
     {}
   );
-
   const items = Object.keys(counter)
     .map((key) => [Number(key), counter[key]])
     .sort((a, b) => b[1] - a[1]);
-
   if (items[0][1] === items?.[1]?.[1]) {
     return -1;
   }
-
   return items[0][0];
 };
