c161db8e178cba458eb302db3b73dc6edf1f1c08
chanki <coldair426@gmail.com>
2023-02-18 23:53:14 +0900

    실패율 게시글 코드

--- programmers Lv.1 실패율/다른사람풀이.js
+++ programmers Lv.1 실패율/다른사람풀이.js
@@ -1,5 +1,5 @@
 function solution(N, stages) {
-  let result = [];
+  const result = [];
   for (let i = 1; i <= N; i++) {
     let reach = stages.filter((x) => x >= i).length;
     let curr = stages.filter((x) => x === i).length;
